e3a00a87af35186669ac4c6c48e0485706f42ba5
Administrator <109837883+cuidnuice@users.noreply.github.com>
2023-01-28 17:33:14 +0900

    Unity

--- My project/Assets/Sound/Script/Control.cs
+++ My project/Assets/Sound/Script/Control.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Control : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5.0f;
+    private CharacterController control;
+
     void Start()
     {
-        
+        control = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(x,0,z).normalized;
+
+        control.SimpleMove(direction * speed);
     }
 }
